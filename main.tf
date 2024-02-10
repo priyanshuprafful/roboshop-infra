c3817738c3e3ea6fa0e69c26b06be4607c053ece
priyanshuprafful <priyanshuprafful@gmail.com>
2024-02-10 17:00:53 +0530

    updating ./roboshop-infra

--- main.tf
+++ main.tf
@@ -13,6 +13,8 @@ module "vpc" {
 
   env = var.env
   tags = var.tags
+  default_route_table = var.default_route_table
+  default_vpc_id = var.default_vpc_id
   for_each = var.vpc
   vpc_cidr = each.value["vpc_cidr"]
   public_subnets = each.value["public_subnets"]
