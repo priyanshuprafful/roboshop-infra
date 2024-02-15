6f5df75ad3c0986210f33b6d05a67a3607762bf5
priyanshuprafful <priyanshuprafful@gmail.com>
2024-02-15 16:31:09 +0530

    updating ./roboshop-infra

--- locals.tf
+++ locals.tf
@@ -3,5 +3,6 @@ locals {
 # private_subnet_idds = [ for k , v in module.vpc["main"].private_subnets : v.id ]
  # private_subnet_idds = [ for k , v in module.vpc["main"].private_subnets["db-az1"] : v.id ]
   db_subnet_ids = tolist([module.vpc["main"].private_subnets["db-az1"].id , module.vpc["main"].private_subnets["db-az2"].id ])
-
+  web_subnet_ids = tolist([module.vpc["main"].private_subnets["web-az1"].id , module.vpc["main"].private_subnets["web-az2"].id ])
+  app_subnet_ids = tolist([module.vpc["main"].private_subnets["app-az1"].id , module.vpc["main"].private_subnets["app-az2"].id ])
 }
