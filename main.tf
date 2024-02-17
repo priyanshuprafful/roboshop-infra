2a825476660845a9da9f59ba91244f0b77cd3cf7
priyanshuprafful <priyanshuprafful@gmail.com>
2024-02-17 17:17:08 +0530

    updating ./roboshop-infra

--- main.tf
+++ main.tf
@@ -49,9 +49,9 @@ module "rds" {
   tags = var.tags
   subnet_ids = local.db_subnet_ids
 
-  for_each = var.rds
-  engine = each.value["engine"]
-  engine_version = each.value["engine_version"]
+  for_each                = var.rds
+  engine                  = each.value["engine"]
+  engine_version          = each.value["engine_version"]
   backup_retention_period = each.value["backup_retention_period"]
   preferred_backup_window = each.value["preferred_backup_window"]
   instance_class          = each.value["instance_class"]
