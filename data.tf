f1e1512002116f93a861cae27d2f064352dab4be
priyanshuprafful <priyanshuprafful@gmail.com>
2024-06-26 03:52:24 +0530

    updating ./roboshop-infra

--- data.tf
+++ data.tf
@@ -2,9 +2,9 @@ data "aws_ssm_parameter" "ssh_pass" {
   name = "${var.env}.ssh.pass"
 }
 
-#data "aws_ami" "ami" {
-#  most_recent = true
-#  name_regex = "devops-practice-with-ansible-my-local-image"
-#  owners = ["self"]
-#
-#} since we are moving to container and k8s , we have deleted the ami's that we created and also the snapshots
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex = "devops-practice-with-ansible-my-local-image"
+  owners = ["self"]
+
+} #since we are moving to container and k8s , we have deleted the ami's that we created and also the snapshots
